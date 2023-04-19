982729ab5dec07e302b84dd64d8d5185cb685893
okarsono <okarsono@raileurope.com>
2023-04-19 12:56:23 +1000

    Showing duration of tokenization event

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
 import "./styles.css";
 import { Frames, CardNumber, ExpiryDate, Cvv } from "frames-react";
+import { useState } from "react";
 
 export default function App() {
+  const [timing, setTiming] = useState({show: false, prev: 0, now: 0});
+
+  const handleClick = () => {
+    console.log(`[${Date.now()}] starting tokenization`);
+    setTiming((timing) => ({...timing, show: false, prev: timing["now"], now: Date.now()}));
+    Frames.submitCard();
+  }
+
+  const tokenizationListener = (e) => {
+    setTiming((timing) => { return {...timing, show: true, prev: timing["now"], now: Date.now()}});
+    console.log(`[${Date.now()}] tokenized ${e.token}`);
+  }
+
   return (
     <div className="App">
       <Frames
         config={{
-          publicKey: "pk_sbox_ogynfaoply5o6ksuw3d3hcee3ez",
-          modes: ['cvv_optional'],
+          publicKey: "pk_sbox_4v4makjv7vb24jdkn4ugtdu5jmc",
+          modes: ['cvv_hidden'],
         }}
-        cardTokenized={(e) => {
-          alert(e.token);
-        }}
+        cardTokenized={tokenizationListener}
       >
         <CardNumber />
         <div className="date-and-code">
@@ -20,13 +32,23 @@ export default function App() {
 
         <button
           id="pay-button"
-          onClick={() => {
-            Frames.submitCard();
-          }}
+          onClick={handleClick}
         >
           PAY GBP 25.00
         </button>
       </Frames>
+
+      <div>
+        { timing.show && showTiming(timing) }
+      </div>
     </div>
+  );
+}
+
+function showTiming(timing) {
+  return (
+    <>
+      Tokenization duration = <b>{(timing["now"] - timing["prev"])/1000} sec</b>
+    </>
   );
 }
